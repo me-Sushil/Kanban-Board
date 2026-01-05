664bf68c61c289456cef4f1f13e109ac2e631e1c
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2026-01-05 14:28:55 +0545

    create base structure of the kanban board

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,31 @@ function App() {
   return (
     <>
       <div>
+        <h1>Add Task</h1>
         <div>
+          <input placeholder="add task" />
+          <button>Add</button>
+        </div>
+      </div>
+
+      <div className="board">
+        <div className="todo">
           <h1>To Do</h1>
+          <div className="todo-task">
+            <span>task 1</span>
+          </div>
         </div>
-        <div>
+        <div className="inProgress">
           <h1>In Progress</h1>
+          <div className="inProgress-task">
+            <span>task 1</span>
+          </div>
         </div>
-        <div>
+        <div className="done">
           <h1>Done</h1>
+          <div className="done-task">
+            <span>task 1</span>
+          </div>
         </div>
       </div>
     </>
