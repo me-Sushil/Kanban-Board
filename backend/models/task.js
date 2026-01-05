e9fe7d37fcdfce6ea1764af3bf8281585b7f067d
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2026-01-05 16:48:49 +0545

    Create  task schema

---
+++ backend/models/task.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const taskSchema = new mongoose.Schema(
+  {
+    task: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["todo", "in-progress", "done"],
+      default: "todo",
+    },
+
+    order: {
+      type: Number,
+      default: 0, 
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+model.exports = mongoose.model("Task", taskSchema);
