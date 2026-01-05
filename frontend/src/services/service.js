03d6153d9a298668adefe25d22533c404abbfa02
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2026-01-05 17:17:50 +0545

    pass task and status to backend

--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -2,12 +2,16 @@ import axios from "axios";
 
 const baseUrl = import.meta.env.VITE_URL;
 const getTask = async () => {
-  const response = await axios.get(`${baseUrl}/task`);
+  const response = await axios.get(`${baseUrl}/api/task`);
   return response.data;
 };
 
-const addTask = async (task) => {
-  const response = await axios.post(`${baseUrl}/task`, task);
+const addTask = async (task, status) => {
+  const taskObj = {
+    task: task,
+    status: status,
+  };
+  const response = await axios.post(`${baseUrl}/api/task`, taskObj);
   return response.data;
 };
 
