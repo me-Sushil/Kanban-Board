49cc14affb63a632108a7561da02519688a8dcb8
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2026-01-05 17:19:23 +0545

    create get and post endpoint

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ app.use(express.json());
 app.use(cors());
 
 const config = require("./utils/config");
+const Task = require("./models/task");
+const middleWare = require("./utils/middleware");
 
 mongoose
   .connect(config.MONGODB_URL)
@@ -17,7 +19,47 @@ mongoose
   .catch((error) => {
     console.log(error, "Error on Database Connection");
   });
-  
+
+  app.use(middleWare.requestLogger);
+
+app.get("/api/task", async (req, res, next) => {
+  try {
+    const task = await Task.find({});
+    return res.status(200).json({ task });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/api/task", async (req, res, next) => {
+  try {
+    const { task, status } = req.body;
+
+    if (!task === undefined) {
+      return res.status(400).json({
+        message: "Missing required fields: Task",
+      });
+    }
+
+    const newTask = new Task({
+      task,
+      status: status || "todo",
+    });
+
+    const result = await newTask.save();
+
+    res
+      .status(201)
+      .json({ message: "Task created successfully", Task: result });
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+app.use(middleWare.errorhandler);
+app.use(middleWare.unknownEndpoint);
+
 
 app.listen(config.PORT, () => {
   console.log(`Server is running on Port ${config.PORT}`);
